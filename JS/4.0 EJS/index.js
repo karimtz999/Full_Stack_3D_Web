8711a968acc5e4c8d1176aff0c8be7b8fafcd11e
ELFA <140125432+karimtz999@users.noreply.github.com>
2025-03-07 01:02:47 +0000

    3.7.25

---
+++ JS/4.0 EJS/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+const today = new Date();
+const day = today.getDay();
+console.log(today);
+console.log(day);
+
+
+let type = "a weekday";
+let adv = "it's time to work hard";
+
+if (day === 0 || day === 6) {
+    type = "the weekend";
+    adv = "it's time to have some fun";
+}
+app.get("/", (req, res) => {
+    res.render("index.ejs", {dayType: type, advice: adv});
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
+});
